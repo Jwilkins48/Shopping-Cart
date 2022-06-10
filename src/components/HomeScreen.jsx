d6a392dcf0eb8815d3e190d779849c050746491f
Jenna <jenna48071@gmail.com>
2022-06-09 21:59:24 -0500

    added styling for homepage and completed routing

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
+import image from '../images/flowersOnTable.jpg';
 
 function HomeScreen() {
   return (
-    <div>
-      home!!
+    <>
+    <div className='homeContainer'>
+      <div className='mainHomeContainer'>
+        <img className='frontImg' src={image} alt='flowers on table' width="850" height="520"></img>
+      </div>
+      <div className='titleHomeContainer'>
+        <h2 className='messageTitle'>Bouquets for every occasion!</h2>
+        <p className='messageHome'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+            ut labore et dolore magna aliqua. Sit amet est placerat in egestas erat imperdiet sed. Mauris nunc congue
+            nisi vitae suscipit tellus. A condimentum vitae sapien pellentesque  tellus. A condimentum vitae sapien pellentesque Mauris nunc congue Maur Mauris.
+          </p>
+          <Link to='/shop' className='orderBtn'>Order now</Link>
+      </div>
     </div>
+    </>
   )
 }
 
